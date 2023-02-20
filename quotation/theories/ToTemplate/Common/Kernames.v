78a6d6119bc3f34cedb1440f618b9222b24c2186
Jason Gross <jgross@mit.edu>
2023-02-19 19:50:21 -0800

    WIP

--- quotation/theories/ToTemplate/Common/Kernames.v
+++ quotation/theories/ToTemplate/Common/Kernames.v
@@ -1,7 +1,18 @@
-From MetaCoq.Quotation.ToTemplate Require Export Coq.Init Coq.MSets Coq.FSets bytestring.
+From MetaCoq.Quotation.ToTemplate Require Export Init.
+From MetaCoq.Quotation.ToTemplate Require Export (hints) Coq.Init Coq.MSets Coq.FSets bytestring.
 From MetaCoq.Common Require Import Kernames.
 
 #[export] Instance quote_modpath : ground_quotable modpath := ltac:(induction 1; exact _).
+
+Module qKername <: QuotationOfOrderedType Kername.
+  MetaCoq Run (tmMakeQuotationOfModule false "Kername").
+End qKername.
+(* Error: Universe inconsistency. Cannot enforce Set =
+MetaCoq.Quotation.ToTemplate.Coq.Structures.1 because
+MetaCoq.Quotation.ToTemplate.Coq.Structures.1 < KernameSet.E.t.u0
+<= MetaCoq.Quotation.ToTemplate.Coq.Structures.1 = Set.*)
+
+(T : OrderedType) (M : MSetAVL.MakeSig T) (Import MOrdProperties : OrdPropertiesSig M) (Import qT : QuotationOfOrderedType T) (Import qM : MSetAVL.QuotationOfMake T M) (qMOrdProperties : QuotationOfOrdProperties M MOrdProperties qT).
 
 Module QuoteKernameSet := QuoteMSetAVL Kername KernameSet.
 Export QuoteKernameSet.Instances.
