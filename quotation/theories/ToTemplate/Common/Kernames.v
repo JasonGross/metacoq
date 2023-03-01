6128865d37e7073204f7830ad16a34006eaecf69
Jason Gross <jgross@mit.edu>
2023-03-01 14:32:00 -0800

    WIP

--- quotation/theories/ToTemplate/Common/Kernames.v
+++ quotation/theories/ToTemplate/Common/Kernames.v
@@ -3,6 +3,12 @@ From MetaCoq.Quotation.ToTemplate Require Export Init.
 From MetaCoq.Quotation.ToTemplate Require Export (hints) Coq.Init Coq.MSets Coq.FSets bytestring.
 From MetaCoq.Quotation.ToTemplate.QuotationOf.Common Require Import Kernames.Instances.
 
+#[export] Instance quote_ident : ground_quotable ident := _.
+#[export] Typeclasses Opaque ident.
+#[export] Instance quote_qualid : ground_quotable qualid := _.
+#[export] Typeclasses Opaque qualid.
+#[export] Instance quote_dirpath : ground_quotable dirpath := _.
+#[export] Typeclasses Opaque dirpath.
 #[export] Instance quote_modpath : ground_quotable modpath := ltac:(induction 1; exact _).
 #[export] Instance quote_kername : ground_quotable kername := _.
 #[export] Typeclasses Opaque kername.
