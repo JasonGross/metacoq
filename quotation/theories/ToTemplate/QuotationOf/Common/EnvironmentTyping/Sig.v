0d69c9f38350433c802e373233ae2bf0324743e5
Jason Gross <jgross@mit.edu>
2023-03-23 11:32:18 -0700

    WIP

--- quotation/theories/ToTemplate/QuotationOf/Common/EnvironmentTyping/Sig.v
+++ quotation/theories/ToTemplate/QuotationOf/Common/EnvironmentTyping/Sig.v
@@ -4,8 +4,34 @@ From MetaCoq.Quotation.ToTemplate Require Import Init.
 Module Type QuotationOfLookup (T : Term) (E : EnvironmentSig T) (L : LookupSig T E).
   MetaCoq Run (tmDeclareQuotationOfModule everything (Some export) "L").
 End QuotationOfLookup.
-Locate Module TermUtils.
-Module Type QuotationOfEnvTyping (T : Term) (E : EnvironmentSig T) (TU : TermUtils T E).
+
+(*
+Module Type QuotationOfEnvTyping (T : Term) (E : EnvironmentSig T) (TU : TermUtils T E) (ET : EnvTypingSig T E TU).
+  MetaCoq Run (tmDeclareQuotationOfModule everything (Some export) "ET").
+End QuotationOfEnvTyping.
+*)
+Module Type QuotationOfConversion (T : Term) (E : EnvironmentSig T) (TU : TermUtils T E) (ET : EnvTypingSig T E TU) (C : ConversionSig T E TU ET).
+  MetaCoq Run (tmDeclareQuotationOfModule everything (Some export) "C").
+End QuotationOfConversion.
+
+(*
+Module Type QuotationOfGlobalMaps (T : Term) (E : EnvironmentSig T) (TU : TermUtils T E) (ET : EnvTypingSig T E TU) (C : ConversionSig T E TU ET) (L : LookupSig T E) (GM : GlobalMapsSig T E TU ET C L).
+  MetaCoq Run (tmDeclareQuotationOfModule everything (Some export) "GM").
+End QuotationOfGlobalMaps.
+*)
+Module Type QuotationOfConversionPar (T : Term) (E : EnvironmentSig T) (TU : TermUtils T E) (ET : EnvTypingSig T E TU) (CS : ConversionParSig T E TU ET).
+  MetaCoq Run (tmDeclareQuotationOfModule everything (Some export) "CS").
+End QuotationOfConversionPar.
+
+Module Type QuotationOfTyping (T : Term) (E : EnvironmentSig T) (TU : TermUtils T E) (ET : EnvTypingSig T E TU)
+  (CT : ConversionSig T E TU ET) (CS : ConversionParSig T E TU ET) (Ty : Typing T E TU ET CT CS).
+End QuotationOfTyping.
+
+Module QuotationOfDeclarationTyping (T : Term) (E : EnvironmentSig T) (TU : TermUtils T E)
+  (ET : EnvTypingSig T E TU) (CT : ConversionSig T E TU ET)
+  (CS : ConversionParSig T E TU ET) (Ty : Typing T E TU ET CT CS)
+  (L : LookupSig T E) (GM : GlobalMapsSig T E TU ET CT L) (DT : DeclarationTyping T E TU ET CT CS Ty L GM).
+
 
 Module Type QuoteTerm (T : Term).
   #[export] Declare Instance quote_term : ground_quotable T.term.
